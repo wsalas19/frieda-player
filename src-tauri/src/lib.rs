477341c14b44feadb5e283326d8dcf3563bbcc13
William Salas <willbullet52@gmail.com>
2026-09-12 12:46:10 -0500

    secutiry: remove unused libraries, add csp, security sweep.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,6 @@ fn get_state() -> Option<media::Snapshot> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_autostart::init(
             tauri_plugin_autostart::MacosLauncher::LaunchAgent,
             None,
